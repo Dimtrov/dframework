2bb4b36bf90e4a18323588d935cf17882efeb506
Dimitri Sitchet Tomkeu <dev.dst@gmail.com>
2021-05-22 14:53:30 +0100

    Creation du CLI d'initialisation de l'application

--- system/core/cli/General.php
+++ system/core/cli/General.php
@@ -20,11 +20,8 @@ namespace dFramework\core\cli;
 use Kahlan\Suite;
 use Kahlan\Box\Box;
 use Kahlan\Cli\Kahlan;
-use Ahc\Cli\Helper\Shell;
 use Kahlan\Jit\ClassLoader;
-use dFramework\core\loader\Service;
 use dFramework\core\loader\ClassMapper;
-use dFramework\core\router\RouteCollection;
 
 /**
  * General
@@ -41,7 +38,7 @@ class General extends Cli
 {
     /**
      * Mapping de classes
-     * 
+     *
      * @return Command
      */
     protected function _map() : Command
@@ -62,8 +59,8 @@ class General extends Cli
                     }
                     $cli->start('Service de mapping des classes pour l\'auto-chargement');
                     $cli->task('Recherche de classes en cours de traitement');
-                    
-                    if (!empty($app)) 
+
+                    if (!empty($app))
                     {
                         $mapper = new ClassMapper([\APP_DIR], [
                             'excluded_paths' => [
@@ -80,23 +77,23 @@ class General extends Cli
                         $mapper = new ClassMapper([\SYST_DIR.'dependencies']);
                         $export_file = \SYST_DIR.'constants'.\DS.'.classmap.php';
                     }
-                    
+
                     $mapper->process();
-            
+
                     if ($mapper->export_result_in_file($export_file))
                     {
                         $cli->io->info("\t => Traitement terminé avec succès.", true);
                         sleep(1.5);
                         $cli->io->writer()->colors("\t => <boldGreen>".count($mapper->get_result_as_array())."</end> <white>classes collectées avec succès</end>");
                     }
-                    else 
+                    else
                     {
                         $cli->io->error("\t Une erreur s'est produite lors de la collecte des classes \n");
                     }
-            
+
                     sleep(1.5);
                     $cli->end();
-                } 
+                }
                 catch (\Throwable $th) {
                     $cli->showError($th);
                 }
@@ -106,49 +103,8 @@ class General extends Cli
     }
 
     /**
-     * Serveur de developpement
-     * 
-     * @return Command
-     */
-    protected function _serve() : Command
-    {
-        return (new Command('serve', 'Demarre le serveur de developpement.'))
-            ->option('--host', 'Hote sur lequel votre application sera lancée. "localhost" par defaut', null, 'localhost')
-            ->option('--port', 'Port sur lequel vous souhaitez demarrer le serveur. "3200" par defaut', null, 3200)
-            ->option('--php', 'Chemin vers l\'executable php à utiliser pour démarrer le serveur.', null, PHP_BINARY)
-            ->action(function($host, $port, $php) { 
-                /**
-                 * @var Command
-                 */
-                $cli = $this;
-                try {
-                    $cli->start('Service de lancement du serveur de developpement');
-                    $cli->task('Demarrage du serveur de developpement');
-
-                    sleep(2);
-                    $cli->io->ok("\t => Le serveur a démarré avec succès.", true);
-                    
-                    sleep(2.5);
-                    $cli->io->writer()->colors("\t => <white>Ouvrez votre navigateur a l'adresse</end> <boldGreen><http://".$host.":".$port."></end>");
-            
-                    sleep(1.5);
-                    $cli->end();
-
-                    $shell = new Shell($php . ' -S '. $host . ':' . $port . ' -t ' . escapeshellarg(\WEBROOT));
-                    $shell->setOptions(dirname(\WEBROOT), null, 2.5)->execute()->isRunning();
-                    
-                    $shell->stop();
-                    $shell->kill();
-                } 
-                catch (\Throwable $th) {
-                    $cli->showError($th);
-                }
-            });
-    }
-
-    /**
      * Test unitaire
-     * 
+     *
      * @return Command
      */
     protected function _test() : Command
@@ -165,7 +121,7 @@ class General extends Cli
 
                     error_reporting(E_ALL);
                     $kahlan_dir = SYST_DIR.'dependencies'.DS.'kahlan'.DS.'kahlan';
-                    
+
                     $autoload = require  $kahlan_dir.'/autoload.php';
                     $autoloader = $autoload("{$kahlan_dir}/vendor");
                     require $kahlan_dir. '/src/functions.php';
@@ -186,7 +142,7 @@ class General extends Cli
                         '--reporter=verbose'
                     ]);
                     \initKahlanGlobalFunctions();
-        
+
                     if ($autoloader instanceof ClassLoader) {
                         $commandLine = $specs->commandLine();
                         $autoloader->patch([
@@ -207,83 +163,12 @@ class General extends Cli
                                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
                         }
                     }
-    
+
                     $specs->run();
                     exit($specs->status());
-                    
-                    $cli->end();
-                }
-                catch (\Throwable $th) {
-                    $cli->showError($th);
-                }
-            });
-    }
-
-    /**
-     * Liste des routes
-     *
-     * @return Command
-     */
-    protected function _routes() : Command 
-    {
-        return (new Command('routes:list', 'Affiche tous les itinéraires définis par l\'utilisateur.'))
-            ->action(function() {
-                /**
-                 * @var Command
-                 */
-                $cli = $this;
-                try {
-                    $cli->start('Service de gestion de l\'application');
-                    $cli->task("Recherche des routes");
-                    $cli->io->write("\n");
-
-                    sleep(1.75);
-
-                    require_once APP_DIR . 'config' . DS . 'routes.php';
-                    $collection = $routes;
-                    $methods    = [
-                        'get',
-                        'head',
-                        'post',
-                        'patch',
-                        'put',
-                        'delete',
-                        'options',
-                        'trace',
-                        'connect',
-                        'cli',
-                    ];
-
-                    $table = [];
-                    foreach ($methods as $method)
-                    {
-                        $routes = $collection->getRoutes($method, true);
-
-                        foreach ($routes as $route => $handler)
-                        {
-                            $tab = [
-                                'Method' => strtoupper($method),
-                                'Route' => $route,    
-                                'Name' => '',
-                                'Handler' => ''
-                            ];
-                            if(is_string($handler))
-                            {
-                                $tab['Handler'] = $handler;
-                            }
-                            if(is_array($handler))
-                            {
-                                $tab['Handler'] = is_string($handler['handler']) ? $handler['handler'] : 'Closure';
-                                $tab['Name'] = $handler['name'];
-                            }
-
-                            $table[] = $tab;
-                        }
-                    }
-                    $cli->io->table($table);
 
                     $cli->end();
-                } 
+                }
                 catch (\Throwable $th) {
                     $cli->showError($th);
                 }
