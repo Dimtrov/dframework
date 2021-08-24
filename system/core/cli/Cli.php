cd319ab5bdbe86360b0669956a77972cfeb93be2
Dimitric Sitchet Tomkeu <dimitrisitchet@gmail.com>
2021-08-24 11:35:11 +0100

    fix: modification du message des exception de CLI
    la classe accusée dans le message n'etait pas reelement celle responsable
    
    utilisation de get_class pour recuperer la classe au lieu de __class__

--- system/core/cli/Cli.php
+++ system/core/cli/Cli.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * dFramework
  *
@@ -7,12 +7,12 @@
  * This content is released under the Mozilla Public License 2 (MPL-2.0)
  *
  * @package	    dFramework
- * @author	    Dimitri Sitchet Tomkeu <dev.dst@gmail.com>
+ * @author	    Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
  * @copyright	Copyright (c) 2019 - 2021, Dimtrov Lab's. (https://dimtrov.hebfree.org)
  * @copyright	Copyright (c) 2019 - 2021, Dimitri Sitchet Tomkeu. (https://www.facebook.com/dimtrovich)
  * @license	    https://opensource.org/licenses/MPL-2.0 MPL-2.0 License
  * @link	    https://dimtrov.hebfree.org/works/dframework
- * @version     3.3.0
+ * @version     3.3.4
  */
 
 namespace dFramework\core\cli;
@@ -26,17 +26,17 @@ use dFramework\core\exception\Exception;
  * @package		dFramework
  * @subpackage	Core
  * @category    Cli
- * @author		Dimitri Sitchet Tomkeu <dev.dst@gmail.com>
+ * @author		Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
  * @link		https://dimtrov.hebfree.org/docs/dframework/guide/Validator.html
  * @since       3.3.0
  * @file        /system/core/cli/Cli.php
  */
-abstract class Cli 
-{   
-    public static function __callStatic($name, $arguments)
+abstract class Cli
+{
+    public static function __callStatic(string $name, array $arguments)
     {
         $instance = new static;
-        
+
         if (method_exists($instance, $name))
         {
             return call_user_func_array([$instance, $name], $arguments);
@@ -45,6 +45,6 @@ abstract class Cli
         {
             return call_user_func_array([$instance, '_'.$name], $arguments);
         }
-        throw new Exception("Method < " .__CLASS__.'::'.$name. "() > not found");
+        throw new Exception("Method < " .get_class($instance).'::'.$name. "() > not found");
     }
 }
