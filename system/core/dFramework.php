91e3f22352bbfc265116acd3126c17a7758d610c
Dimitric Sitchet Tomkeu <dimitrisitchet@gmail.com>
2021-08-25 16:09:56 +0100

    Mise a jour du numero de version

--- system/core/dFramework.php
+++ system/core/dFramework.php
@@ -12,7 +12,7 @@
  *  @copyright	Copyright (c) 2019 - 2021, Dimitri Sitchet Tomkeu. (https://www.facebook.com/dimtrovich)
  *  @license    https://opensource.org/licenses/MPL-2.0 MPL-2.0 License
  *  @link	    https://dimtrov.hebfree.org/works/dframework
- *  @version    3.3.2
+ *  @version    3.3.4
  */
 
 namespace dFramework\core;
@@ -38,7 +38,7 @@ use MirazMac\Requirements\Checker As envChecker;
  */
 class dFramework
 {
-    const VERSION = '3.3.2';
+    const VERSION = '3.3.4';
 
 	/**
 	 * @var array Liste des extensions requises pour le fonctionnement du framework
